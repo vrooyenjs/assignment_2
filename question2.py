2eeaa7c7cb2a20b348b99d2f47f142e842ec4add
vrooyenjs <csr.spies@gmail.com>
2022-08-16 22:27:37 +0200

    Question 2.1, part b.

--- question2.py
+++ question2.py
@@ -7,12 +7,14 @@ from collections import Counter
 from nltk.corpus import brown
 import nltk
 
+# File to tag
+FILE_TO_TAG='question_2_resources/pos_test.txt'
 
 # To enable debug logging, change to True
 # 0 - INFO
 # 1 - DEBUG
 # 2 - FINE
-LOG_LEVEL = 1
+LOG_LEVEL = 2
 UNIQUE_WORD_LIST = set()
 
 # Since this is a much larger corpus than Unisa's one, we limit the words to process.
@@ -39,8 +41,8 @@ def get_distinct_words(corpus):
 
 # Function used to determine the most likely POS tag used for a unique word
 def arg_max_for_word(corpus, word):
-    if LOG_LEVEL > 1:
-        print('Starting word_processor')
+    if LOG_LEVEL > 2:
+        print('Starting arg_max_for_word')
 
     # Create an array of all the tags in the corpus that the given word was tagged with
     tag_counts = []
@@ -65,10 +67,10 @@ def arg_max_for_word(corpus, word):
         print(word, end=' --> ')
         print(word_statistics, end=' --> ')
         print(arg_max_pos_tag)
-    if LOG_LEVEL > 1:
-        print('Ending word_processor')
+    if LOG_LEVEL > 2:
+        print('Ending arg_max_for_word')
     return arg_max_pos_tag
-# end def word_processor(corpus, word)
+# end def arg_max_for_word(corpus, word)
 
 
 # Function used to compute the argmax function for each word
@@ -80,7 +82,7 @@ def argmax():
 
     # Get a list of unique words.
     distinct_word_list = get_distinct_words(brown_sentences)
-    arg_max_for_word(brown_sentences, distinct_word_list)
+    # arg_max_for_word(brown_sentences, distinct_word_list)
 
     # The sentences provided are already split up into a 2-d arrays.
     counter = 0
@@ -96,7 +98,6 @@ def argmax():
 
     if LOG_LEVEL > 1:
         print('Ending argmax')
-
 # end def argmax ()
 
 
@@ -112,6 +113,37 @@ def download_corpus():
 # end def download_corpus()
 
 
+# Function used to go through all the words in a given file and tag them as per the
+# MLE of the POS tag given the brown corpus
+def tag_file(file):
+    if LOG_LEVEL > 1:
+        print('Staring tag_file')
+    input_file = open(file, "r")
+    output_file = open(file + '_out', "w")
+
+    # Grab the POS tagged sentences with from the 'news' category.
+    brown_sentences = brown.tagged_sents(categories="news")
+
+    for line in input_file:
+        tagged_line = ''
+        for word in line.split():
+            pos_tag = arg_max_for_word(brown_sentences, word)
+
+            # If we do not find any words that match, that means the given word is not in the corpus.
+            # Use the tag 'NN' instead.
+            if not pos_tag:
+                pos_tag = 'NN'
+            tagged_line = tagged_line + word+ '/' +pos_tag + ' '
+        tagged_line = tagged_line + '\n'
+        output_file.write(tagged_line)
+        if LOG_LEVEL > 1:
+            print(tagged_line)
+
+    if LOG_LEVEL > 1:
+        print('Ending tag_file')
+# end def tag_file(FILE_TO_TAG)
+
+
 # Functions used as the main entry into the application
 def main():
     if LOG_LEVEL > 1:
@@ -119,7 +151,12 @@ def main():
 
     # No file available via the MyUnisa portal, downloading the standard Brown Corpus
     download_corpus()
-    argmax()
+
+    # Part a
+    # argmax()
+
+    # Part b
+    tag_file(FILE_TO_TAG)
 
     if LOG_LEVEL > 1:
         print('Ending main')
